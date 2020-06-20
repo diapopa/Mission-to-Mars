4232b8622480acd191901666456b3c3d02b45883
diapopa <dia.popa@hotmail.com>
2020-06-20 12:42:46 -0400

    Use flask to create a web app.

---
+++ apps/app.py
@@ -0,0 +1,33 @@
+# Use Flask to render a template
+from flask import Flask, render_template
+
+# Use PyMongo to interact with our Mongo database
+from flask_pymongo import PyMongo
+
+# Use the scraping code
+import scraping
+
+# Set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Define the route for the HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# Set up our scraping route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+# Run flask
+if __name__ == "__main__":
+   app.run()
